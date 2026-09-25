cb70df88d6df0545cfc23bb100b6eaf500a4c4e5
Kawabuchi-Takeru <omikoshikagura0@gmail.com>
2026-09-25 16:50:42 +0900

    feat: apiに送信するまで

--- web/js/api.js
+++ web/js/api.js
@@ -1,17 +1,29 @@
-const preview = document.getElementById("preview");
+// アップロードボタンとファイル選択部分
 const upload = document.getElementById("upload");
 const filePlace =document.getElementById("filePlace");
 
-filePlace.addEventListener("change",function(){
+// 変更したら入れようとか思ったけどボタンを押したらひっぱるでよかった子たち
+// filePlace.addEventListener("change",function(){
+//     const file = filePlace.files[0];
+// });
+
+upload.addEventListener("click",async () => {
+    // 画像入れます
     const file = filePlace.files[0];
+    // でーたのいれものつくります
+    const formData = new FormData();
+    // つくったいれものにがぞういれます
+    formData.append("image", file)
+    // goのapiにむけて画像はいったいれものをhttpつうしんでおくる
+    const response = await fetch(
+        "http://localhost:3000/",
+        {
+            method: "POST",
+            bosy: formData
+        }
+    );
 });
 
-const formData = new FormData();
 
-formData.append("image", file)
-
-upload.addEventListener("click",function(){
-
-});
-
+const preview = document.getElementById("preview");
 preview.src = ここにpreviewのURLをいれる;
